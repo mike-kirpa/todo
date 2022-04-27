a28cccc322b5560c07a162453d4a1d3436fb669c
Mikhail Kyrpa <michael.kirpa@gmail.com>
2022-04-27 19:01:55 +0300

    add service and repository

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/mike-kirpa/just-todo"
 	"github.com/mike-kirpa/just-todo/pkg/handler"
+	"github.com/mike-kirpa/just-todo/pkg/repository"
+	"github.com/mike-kirpa/just-todo/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,9 +2,15 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mike-kirpa/just-todo/pkg/service"
 )
 
 type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
