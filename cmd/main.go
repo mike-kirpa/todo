7815d89932dfaff5f8026731a73a85b928ccec5d
Mikhail Kyrpa <michael.kirpa@gmail.com>
2022-05-30 18:32:27 +0300

    Connect to DB. Env variables. Sqlx library.

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-	"github.com/mike-kirpa/just-todo"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	todo "github.com/mike-kirpa/just-todo"
 	"github.com/mike-kirpa/just-todo/pkg/handler"
 	"github.com/mike-kirpa/just-todo/pkg/repository"
 	"github.com/mike-kirpa/just-todo/pkg/service"
-	"log"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err)
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
@@ -17,4 +42,10 @@ func main() {
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error uccured while runnung http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
